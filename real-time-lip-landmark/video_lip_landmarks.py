aa8ecdeabd771ab7972e0c31427ced33e9b6fcaa
byeonghuny <sbhbins@naver.com>
2019-10-01 08:54:25 +0900

    real time lip detection and landmark

---
+++ real-time-lip-landmark/video_lip_landmarks.py
@@ -0,0 +1,104 @@
+# USAGE
+# python video_lip_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat
+
+# import the necessary packages
+from imutils.video import VideoStream
+from collections import OrderedDict
+from imutils import face_utils
+import datetime
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+# define a dictionary that maps the indexes of the facial
+# landmarks to specific face regions
+FACIAL_LANDMARKS_INDEXES = OrderedDict([
+    ("Mouth_out", (48, 60)),
+])
+
+facial_features_cordinates = {}
+
+def visualize_facial_landmarks(frame, shape, colors=None, alpha=0.75):
+    # create two copies of the input image -- one for the
+    # overlay and one for the final output image
+    overlay = frame.copy()
+    output = frame.copy()
+
+    # if the colors list is None, initialize it with a unique
+    # color for each facial landmark region
+    if colors is None:
+        colors = [(19, 199, 109)]
+
+    # loop over the facial landmark regions individually
+    for (i, name) in enumerate(FACIAL_LANDMARKS_INDEXES.keys()):
+        # grab the (x, y)-coordinates associated with the
+        # face landmark
+        (j, k) = FACIAL_LANDMARKS_INDEXES[name]
+        pts = shape[j:k]
+        facial_features_cordinates[name] = pts
+
+        # otherwise, compute the convex hull of the facial
+        # landmark coordinates points and display it
+        hull = cv2.convexHull(pts)
+        cv2.drawContours(overlay, [hull], -1, colors[i], -1)
+
+    # apply the transparent overlay
+    cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+
+    # return the output image
+    print(facial_features_cordinates)
+    return output
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-r", "--picamera", type=int, default=-1,
+	help="whether or not the Raspberry Pi camera should be used")
+args = vars(ap.parse_args())
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] camera sensor warming up...")
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+time.sleep(0.5)
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream, resize it to
+	# have a maximum width of 400 pixels, and convert it to
+	# grayscale
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# detect faces in the grayscale frame
+	rects = detector(gray, 0)
+
+	# loop over the face detections
+	for (i, rect) in enumerate(rects):
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		frame = visualize_facial_landmarks(frame, shape)
+
+	# show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
